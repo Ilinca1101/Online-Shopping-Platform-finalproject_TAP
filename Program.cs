5518a1c5578a471e3d05be9fac31b6bb86cd5cbb
Ilinca1101 <162047237+Ilinca1101@users.noreply.github.com>
2024-05-19 19:58:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,42 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Proiect1;
-using Proiect1.Services;
-using Proiect1.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Repositories.Contracts;
+using ShopOnline.Api.Repositories;
+using Microsoft.Net.Http.Headers;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7244/") });
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
+// Add services to the container.
 
-await builder.Build().RunAsync();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyDbContext>(options =>
+{ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("ShopOnline.Api")); }
+);
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(policy =>
+policy.WithOrigins("http://localhost:7293", "https://localhost:7293")
+.AllowAnyMethod()
+.WithHeaders(HeaderNames.ContentType)
+);
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
